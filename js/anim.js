9f9159b87e03ae39435f3b9d890662bd257c3425
Adolfo Bastardo <adolfo.bastardo@gmail.com>
2016-04-21 15:58:58 -0430

    Añadiendo Desarrollo a Servicios

--- js/anim.js
+++ js/anim.js
@@ -17,3 +17,87 @@ $(document).ready(function () {
 
     }
 });
+
+/* Navegación */
+			[].slice.call( document.querySelectorAll( '.dotstyle > ul' ) ).forEach( function( nav ) {
+				new DotNav( nav, {
+					callback : function( idx ) {
+						var c = document.getElementById("dotsa").children;
+                        var url = c[idx].children[0].href;
+                        window.location = url;
+					}
+				} );
+			} );
+            
+            
+            $("div").mouseover(function(e){
+                
+                if(e.currentTarget.id == "present" || e.currentTarget.id == "quienes" || e.currentTarget.id == "servi" || 
+                e.currentTarget.id == "tech" || e.currentTarget.id == "contacto") {
+                    if(e.currentTarget.id == "present"){
+                        $("#nHome").siblings("li").removeClass("current");
+                        $("#nHome").addClass("current");
+                    }
+                    else if(e.currentTarget.id == "quienes"){
+                        $("#nWe").siblings("li").removeClass("current");
+                        $("#nWe").addClass("current");
+                    }
+                    else if(e.currentTarget.id == "servi"){
+                        $("#nServi").siblings("li").removeClass("current");
+                        $("#nServi").addClass("current");
+                    }
+                    else if(e.currentTarget.id == "tech"){
+                        $("#nTech").siblings("li").removeClass("current");
+                        $("#nTech").addClass("current");
+                    }
+                    else if(e.currentTarget.id == "contacto"){
+                        $("#nCont").siblings("li").removeClass("current");
+                        $("#nCont").addClass("current");
+                    }
+                }
+            });
+            
+            /* Quienes Somos */
+            
+            
+            $(".img1").click(function(e){
+                console.log(e.currentTarget.id);
+                $(e.currentTarget).removeClass("bounceIn");
+                $(e.currentTarget).siblings(".img2").removeClass("zoomOut");
+                $(e.currentTarget).addClass("zoomOut");
+                setTimeout(function(){$(e.currentTarget).addClass("hidden");}, 1000);
+                setTimeout(function(){$(e.currentTarget).siblings(".img2").removeClass("hidden");}, 1000);
+                $(e.currentTarget).siblings(".img2").addClass("bounceIn");
+            });
+            $(".img2").click(function(e){
+                $(e.currentTarget).removeClass("bounceIn");
+                $(e.currentTarget).siblings(".img1").removeClass("zoomOut");
+                $(e.currentTarget).addClass("zoomOut");
+                setTimeout(function(){$(e.currentTarget).addClass("hidden");}, 1000);
+                setTimeout(function(){$(e.currentTarget).siblings(".img1").removeClass("hidden");}, 1000);
+                $(e.currentTarget).siblings(".img1").addClass("bounceIn");
+            });
+            
+            /* Servicios */
+            
+            
+            $(".diseño").hover(function(e){
+                $("#dText").removeClass("hidden zoomOutDown");
+                $("#dText").addClass("zoomInUp");
+            },function(e){
+                $("#dText").removeClass("zoomInUp");
+                $("#dText").addClass("zoomOutDown", 1000, "easeInBack");
+                $("#dText").addClass("hidden", 1000);
+            });
+            
+            
+            $(".desarrollo").hover(function(e){
+                $("#dvText").removeClass("hidden zoomOutDown");
+                $("#dvText").addClass("zoomInUp");
+            },function(e){
+                $("#dvText").removeClass("zoomInUp");
+                $("#dvText").addClass("zoomOutDown", 1000, "easeInBack");
+                $("#dvText").addClass("hidden", 1000);
+            });
+            
+            
